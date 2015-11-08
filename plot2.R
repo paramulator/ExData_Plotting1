45febeb9d4c778170629a8532a2cfe9c24a4e30a
Stan Williams <saw.williams@sbcglobal.net>
2015-11-08 10:24:07 -0600

    Improvements to download and data frame steps

--- plot2.R
+++ plot2.R
@@ -1,7 +1,14 @@
-# Load dplyr for data frame manipulations.
-library(dplyr)
+# Plot 2 -----------------------------------------------------------------------
 
-# Source and local zip files.
+
+
+# STEP 1: Get packages and set pointers for various files and folders we need -- 
+
+# Load dplyr for data frame manipulations.  Won't reload if already loaded.
+library(dplyr)
+library(lubridate)
+
+# External and local zip files.
 externalZipURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 localZipFile <- "___Fhousehold_power_consumption___.zip"
 
@@ -11,39 +18,39 @@ rootFolder <- "UCI Power Dataset"
 # Unzipped data file name in root folder.  Appears to be a text file.
 dataFileName <- "household_power_consumption.txt"
 
-# Date range we are using.
+# Date range we care about for this assignment.
 dateRange <- c("1/2/2007", "2/2/2007")
 
-makePOSIX <- function(dt, tm) {
-# Utility function for converting raw date and time strings into POSIXct.
-# 
-# Inputs: 
-#   dt: a character string of the form "dd/mm/yy".    
-#   tm: a character string of the form "hh:mm:ss".
-#   
-# Outputs:
-#   A POSIXct date/time value
-#   
-    as.POSIXct(strptime(paste(dt, tm), format = "%d/%m/%Y %H:%M:%S"))    
-}
 
-# STEP 1: Download raw data if we need to --------------------------------------
+
+# STEP 2: Download raw data if we need to --------------------------------------
+
 # If a folder called "UCI Power Dataset" does not already exist in the current 
-# working directory, download the data and unzip it into this directory.  If 
-# the folder already exists it will be assumed that the data have already 
-# been downloaded.
+# working directory, download the data and unzip it into this folder.  If the 
+# folder already exists we'll skip this step since it so time consuming and
+# assume that the data have already been properly downloaded.
 
 download <- FALSE
 if (!file.exists(rootFolder)) {
-    print("Dowloading source data")
+    message("Dowloading source data")
     download.file(externalZipURL, localZipFile)
     unzip(localZipFile, exdir = rootFolder) 
     file.remove(localZipFile)
     download <- TRUE
 }
 
-# STEP 2: If we need to, read the data, subset, and create time stamp ----------
+
+
+# STEP 3: Read unzipped data if we need to -------------------------------------
+
+# If the powerData df already exists, we'll skip this step since it is so
+# time consuming and since we're using the same code to read the same data for
+# multiple plots.  If the powerData df does not exist, or we just downloaded the 
+# raw data file, then go ahead and rebuild this df.
+
 if (!exists("powerData") | download == TRUE) {
+    message("Building powerData data frame")
+    
     # Full path from current working directory to the raw data file.
     dataPath <- paste0(rootFolder, "/", dataFileName) 
     
@@ -54,11 +61,15 @@ if (!exists("powerData") | download == TRUE) {
                                            rep("numeric", 7))) %>%
         tbl_df %>%
         filter(Date %in% dateRange) %>%
-        mutate(timeStamp = makePOSIX(Date, Time)) %>%  # Using function above.
+        mutate(timeStamp = dmy_hms(paste(Date, Time))) %>% 
         select(timeStamp, everything(), -Date, -Time)      
 }
 
-# STEP 3: Build Plot #2  -------------------------------------------------------
+
+
+# STEP 4: Build Plot #2  -------------------------------------------------------
+
+# Set dimensions of plot; specify file for output in current working directory.
 png(width = 480, height = 480, filename = "plot2.png")
 
 par(mar = c(4, 4, 4, 2), oma = c(0, 0, 0, 0), cex = 1.0)
